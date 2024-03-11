2caa8ac321e0f2b1be8e89dfe71a60a1f16ca059
Raish10100 <raishgma101001@gmail.com>
2024-03-11 12:34:41 +0530

    Random-Color-Genrator project complete

--- Random-Color-Genrator/script.js
+++ Random-Color-Genrator/script.js
@@ -1,1 +1,19 @@
- 
+ function getColor(){
+    const colorNumber = Math.floor(Math.random() * 16777215);
+    const colorCode = "#" + colorNumber.toString(16);
+
+    document.body.style.backgroundColor = colorCode;
+
+    navigator.clipboard.writeText(colorCode);
+
+    document.querySelector('#color-code').innerText = colorCode;
+
+ }
+
+ const btn = document.getElementById("btn");
+ btn.addEventListener(
+    'click',
+    getColor
+ )
+
+ getColor()
